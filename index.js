154c474ac668bc450ed0df8f3e94b5733682420d
Joseph Schmitt <codewaggle@gmail.com>
2023-04-03 00:53:57 -0400

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+function findMatching(namesArray, driverName) {
+    let results = namesArray.filter((sName) => { 
+        return sName.toLowerCase() === driverName.toLowerCase();
+    })
+
+    return results
+}
+
+
+function fuzzyMatch(namesArray, driverNameBegin) {
+    let results = namesArray.filter((sName) => {  
+        return sName.substring(0, 1) === driverNameBegin.substring(0, 1);
+    })
+    
+    return results
+}
+
+
+function matchName(namesArray, driverName) {
+    const returnArray = []
+
+    namesArray.forEach(nameSet => {
+        if (nameSet.name == driverName) {
+            returnArray.push(nameSet)
+        }  
+    });
+
+    return returnArray
+}
